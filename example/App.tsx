2dd4311a31b98bd819279e6955aa4c29daf23471
FreakyCoder <kurayogun@gmail.com>
2020-09-16 17:21:25 +0300

    Library is ready to deploy

--- example/App.tsx
+++ example/App.tsx
@@ -1,31 +1,36 @@
 import React from "react";
-import { View, StatusBar, SafeAreaView } from "react-native";
+import { View, Text, StatusBar, SafeAreaView } from "react-native";
 import RNMultiSelect, { IMultiSelectDataTypes } from "./lib/RNMultiSelect";
 
 const staticData: Array<IMultiSelectDataTypes> = [
   {
     id: 0,
     value: "Euismod Justo",
+    isChecked: false,
     // imageSource: require("./assets/money.png"),
   },
   {
     id: 1,
     value: "Risus Venenatis",
+    isChecked: false,
     // imageSource: require("./assets/beer.png"),
   },
   {
     id: 2,
     value: "Vestibulum Ullamcorper",
+    isChecked: false,
     // imageSource: require("./assets/party.png"),
   },
   {
     id: 3,
     value: "Lorem Nibh",
+    isChecked: false,
     // imageSource: require("./assets/food-and-restaurant.png"),
   },
   {
     id: 4,
     value: "Ligula Amet",
+    isChecked: false,
     // imageSource: require("./assets/guitar.png"),
   },
 ];
@@ -48,7 +53,13 @@ const App = () => {
             },
           }}
         >
-          <RNMultiSelect data={staticData} onSelect={() => {}} />
+          <RNMultiSelect
+            disableAbsolute
+            data={staticData}
+            onSelect={(selectedItems) =>
+              console.log("SelectedItems: ", selectedItems)
+            }
+          />
         </View>
       </SafeAreaView>
     </>
